503d556f3e12f81f35530f804b63be4f1ea83b3a
William Butts <wbutts1023@gmail.com>
2015-09-22 16:49:51 -0400

    Passing money test

--- textminer/separator.py
+++ textminer/separator.py
@@ -5,7 +5,19 @@ def words(string):
     return v.words(string)
 
 def phone_number(string):
+    phone_book = {}
     if not v.phone_number(string):
         return None
-    else:
-        pass
+    ret = re.sub(r'[^0-9]','',string)
+    phone_book['area_code'] = ret[:3]
+    phone_book['number'] = '{}-{}'.format(ret[3:6],ret[6:])
+    return phone_book
+
+def money(string):
+    money_dict = {}
+    if not v.money(string):
+        return None
+    money_dict['currency'] = string[0]
+    ret = re.sub(r'[^0-9\.]', '', string)
+    money_dict['amount'] = float(ret[0:])
+    return money_dict
