d4ec314aee0550f150047c2f794cff2377858a42
William Butts <wbutts1023@gmail.com>
2015-09-22 17:16:37 -0400

    Passing all regular separator tests

--- textminer/separator.py
+++ textminer/separator.py
@@ -21,3 +21,31 @@ def money(string):
     ret = re.sub(r'[^0-9\.]', '', string)
     money_dict['amount'] = float(ret[0:])
     return money_dict
+
+def zipcode(string):
+    zip_disk = {}
+    if not v.zipcode(string):
+        return None
+    ret = re.sub('[^0-9]','', string)
+    if len(ret) == 9:
+        zip_disk['zip'] = ret[:5]
+        zip_disk['plus4'] = ret[5:]
+    else:
+        zip_disk['zip'] = ret[:5]
+        zip_disk['plus4'] = None
+    return zip_disk
+
+def date(string):
+    dates = {}
+    if not v.date(string):
+        return None
+    ret = re.split(r'[\/\-]', string)
+    if len(ret[0]) == 4:
+        dates['year'] = int(ret[0])
+        dates['month'] = int(ret[1])
+        dates['day'] = int(ret[2])
+    else:
+        dates['year'] = int(ret[2])
+        dates['month'] = int(ret[0])
+        dates['day'] = int(ret[1])
+    return dates
