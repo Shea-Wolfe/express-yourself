c7c1cbdb9a424fb32d026dedcbf2ce865b7c58f4
William Butts <wbutts1023@gmail.com>
2015-09-22 20:46:03 -0400

    Got hard calander test passing, a few cut corners but fairly robust

--- textminer/validator.py
+++ textminer/validator.py
@@ -15,13 +15,13 @@ def words(string, count=None):
     ret = re.split(' ', string)
     if count:
         if count != len(ret):
-            return False
+            return None
         else:
             pass
     if None in [word(s) for s in ret]:
-        return False
+        return None
     else:
-        return True
+        return ret
 
 def phone_number(string):
     return re.match(r'.*\d{3}\W*\d{3}\W*\d{4}$',string)
@@ -34,3 +34,8 @@ def zipcode(string):
 
 def date(string):
     return re.match(r'\d{4}[\/\-]{1}\d{2}[\/\-]{1}\d{2}|\d{1,2}[\/\-]{1}\d{1,2}[\/\-]{1}\d{4}',string)
+
+def hdate(string):
+    return re.match(r'([A-Z]{1}[a-z]*\.? (?=[0-2]\d|[1-9]\W|30|31)|0?2\W*(?=([0-2]\d|[0-9]\W))|(0?1|0?[3-9]|1[0-2])\W*(?=([0-2]\d|30|31|[0-9]\W))|[12][0-9]{3}\W)'
+                    r'([A-Z]{1}[a-z]*\.? (?=[0-2]\d|[1-9]\W|30|31)|0?2\W*(?=([0-2]\d|[0-9]\W))|(0?1|0?[3-9]|1[0-2])\W*(?=([0-2]\d|30|31|[0-9]\W))|[0-2]?[0-9]\W|30\W|31\W)'
+                    r'([0-2]?[0-9]$|30$|31$|[1-2][0-9]{3}$)', string)
