850452e8e3e693eb83cb9610365d323c18eb342f
William Butts <wbutts1023@gmail.com>
2015-09-22 22:34:57 -0400

    Adding updated test files, updated README

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Getting Started
-How to run!
+extractor.py, separator.py, and validator.py are all files holding a number of different RE functions.  They are not meant to be run, so open them with $ atom name to view the functions and see if they would help.
+
+test_extractor.py, test_separator.py, and test_validator.py are all test files for their namesake files.  To run them first make sure you've run $ pip install -r requirements.txt.  Once you've done that enter $ py.test in the directory and it will automatically find and run the tests.
+
 ## Description
-What it does!
+These files are a number of RE functions made to practice finding, extracting, and separating different bits of string data. 
--- textminer/tests/test_separator.py
+++ textminer/tests/test_separator.py
@@ -4,7 +4,7 @@ params = pytest.mark.parametrize
 
 import textminer.separator as s
 
-@xfail
+
 @params("input,expected", [
     ("hello", ['hello']),
     ("hello world", ['hello', 'world']),
@@ -16,7 +16,7 @@ def test_words(input, expected):
     assert s.words(input) == expected
 
 
-@xfail
+
 @params("input,expected", [
     ("919-555-1212", {"area_code": "919", "number": "555-1212"}),
     ("(919) 555-1212", {"area_code": "919", "number": "555-1212"}),
@@ -29,7 +29,6 @@ def test_phone_numbers(input, expected):
     assert s.phone_number(input) == expected
 
 
-@xfail
 @params("input,expected", [
     ("$4", {"currency": "$", "amount": 4.0}),
     ("$19", {"currency": "$", "amount": 19.0}),
@@ -57,7 +56,7 @@ def test_money(input, expected):
     assert s.money(input) == expected
 
 
-@xfail
+
 @params("input,expected", [
     ("63936", {"zip": "63936", "plus4": None}),
     ("50583", {"zip": "50583", "plus4": None}),
@@ -73,7 +72,7 @@ def test_zip(input, expected):
     assert s.zipcode(input) == expected
 
 
-@xfail
+
 @params("input,expected", [
     ("9/4/1976", {"month": 9, "day": 4, "year": 1976}),
     ("1976-09-04", {"month": 9, "day": 4, "year": 1976}),
@@ -84,22 +83,6 @@ def test_zip(input, expected):
 ])
 def test_date(input, expected):
     assert s.date(input) == expected
-
-
-## HARD MODE BEGINS
-
-@xfail
-@params("input,expected", [
-    ("9/4/1976", {"month": 9, "day": 4, "year": 1976}),
-    ("1976-09-04", {"month": 9, "day": 4, "year": 1976}),
-    ("2015-01-01", {"month": 1, "day": 1, "year": 2015}),
-    ("02/15/2004", {"month": 2, "day": 15, "year": 2004}),
-    ("9/4", None),
-    ("2015", None),
-])
-def test_date(input, expected):
-    assert s.date(input) == expected
-
 
 @xfail
 @params("input,expected", [
@@ -110,10 +93,10 @@ def test_date(input, expected):
     ("02/30/2015", None),
 ])
 def test_hard_date(input, expected):
-    assert s.date(input) == expected
+    assert s.hdate(input) == expected
 
 
-@xfail
+
 @params("input,expected", [
     ("stroman.azariah@yahoo.com",
      {"local": "stroman.azariah",
